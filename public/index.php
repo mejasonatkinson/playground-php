e1ad1c443e0aa2a55e1d13db2a155e588e421045
mejasonatkinson <me@jasonatkinson.co.uk>
2022-08-27 20:36:43 +0000

    Doing some basic php

--- public/index.php
+++ public/index.php
@@ -8,7 +8,64 @@
 </head>
 <body>
     <?php 
-        echo "Hello World";
+
+        // this is a single line comment.
+        
+        # this is also a single line comment.
+
+        /*
+        ******************************
+        multiple-lines comment.
+        ******************************
+        */
+
+        $varHelloWorld = "Hello world";
+
+        echo $varHelloWorld . " my name is Jason";
+
+        echo "<br>";
+        echo "<br>";
+
+        $x = 5; 
+        $y = 4; 
+        echo $x + $y;
+
+        // global scope can't us in a function
+        // UNLESS you use the global key word.
+
+        function addXY() {
+            global $x, $y;
+            $y = $x + $y;
+            // OR
+            // $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+        }
+        
+        addXY();
+
+        echo "<br>";
+        echo "<br>";
+
+        echo $y; // outputs 9
+
+        // scoped variables can't be used outside of the function
+        // UNLESS you use the static key word
+
+        echo "<br>";
+        echo "<br>";
+
+
+        function staticZ() {
+            static $z = 0;
+            echo $z;
+            $z++;
+        }
+        
+        staticZ();
+        staticZ();
+        staticZ();
+
+        // https://www.w3schools.com/php/php_echo_print.asp
+
     ?>
 </body>
 </html>
