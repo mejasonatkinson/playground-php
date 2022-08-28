953fd8a6f08a747add7aa1533b33ccab47f922ab
mejasonatkinson <me@jasonatkinson.co.uk>
2022-08-28 11:54:49 +0000

    starting to impliment a structure

--- public/index.php
+++ public/index.php
@@ -4,68 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    <?php 
-
-        // this is a single line comment.
-        
-        # this is also a single line comment.
-
-        /*
-        ******************************
-        multiple-lines comment.
-        ******************************
-        */
-
-        $varHelloWorld = "Hello world";
-
-        echo $varHelloWorld . " my name is Jason";
-
-        echo "<br>";
-        echo "<br>";
-
-        $x = 5; 
-        $y = 4; 
-        echo $x + $y;
-
-        // global scope can't us in a function
-        // UNLESS you use the global key word.
-
-        function addXY() {
-            global $x, $y;
-            $y = $x + $y;
-            // OR
-            // $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
-        }
-        
-        addXY();
-
-        echo "<br>";
-        echo "<br>";
-
-        echo $y; // outputs 9
-
-        // scoped variables can't be used outside of the function
-        // UNLESS you use the static key word
-
-        echo "<br>";
-        echo "<br>";
-
-
-        function staticZ() {
-            static $z = 0;
-            echo $z;
-            $z++;
-        }
-        
-        staticZ();
-        staticZ();
-        staticZ();
-
-        // https://www.w3schools.com/php/php_echo_print.asp
-
-    ?>
+    <ul>
+        <li>
+            <a href="w3school.php">w3school</a>
+        </li>
+    </ul>
 </body>
 </html>
---
+++ public/w3school.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>w3schools Playground</title>
+</head>
+<body>
+    
+    <?php 
+        // Syntax
+        // anything written inbetween these 2 brackets is php 
+    ?>
+
+    <?php 
+        // Comments 
+    ?>
+    
+    <?php
+
+        // Variables
+
+        // Variables scope
+
+        // Output
+
+        // Echo
+        echo "Hello world";
+
+        // Print
+        print "Hello world";
+
+        // Data types:
+
+        // String
+        // Integer
+        // Float (floating point numbers - also called double)
+        // Boolean
+        // Array
+        // Object
+        // NULL
+        // Resource
+
+        // String Functions
+
+        // Length
+        // Count
+        // Reverse
+        // Search
+        // replace
+        // And more...
+        // https://www.w3schools.com/php/php_ref_string.asp
+
+        // Numbers
+
+        // Integers
+        // Floats
+        // Infinity
+        // NaN
+        // Numerical Strings
+        // Casting
+
+        // Math
+        
+        // Pi
+        // min
+        // max
+        // absolute
+        // square root
+        // round
+        // random
+        // And more...
+        // https://www.w3schools.com/php/php_ref_math.asp
+
+        // Constants
+
+        // String
+        // Array
+
+        // Operators
+
+        // Arithmetic operators
+        // Assignment operators
+        // Comparison operators
+        // Increment/Decrement operators
+        // Logical operators
+        // String operators
+        // Array operators
+        // Conditional assignment operators
+
+        // Conditional Statements
+
+        // if statement
+        // if...else statement
+        // if...elseif...else statement
+
+        // switch statement
+
+        // Loops
+
+        // while
+        // do...while
+        // for
+        // foreach
+        // Break 
+        // Continue
+
+        // Arrays
+
+        // Associative arrays
+        // Multidimensional arrays
+        // Two-dimensional arrays
+        // Sorting
+        // And more...
+        // https://www.w3schools.com/php/php_ref_array.asp
+
+        // Superglobals
+
+        // $GLOBALS
+        // $_SERVER
+        // $_REQUEST
+        // $_POST
+        // $_GET
+        // $_FILES
+        // $_ENV
+        // $_COOKIE
+        // $_SESSION
+
+        // Regular expressions
+
+        // Form Handling
+        // Form Validation
+        // Form Required
+        // Form URL/E-mail
+        // Form Complete
+
+
+        // Date and Time
+        // Include
+        // File Handling
+        // File Open/Read
+        // File Create/Write
+        // File Upload
+        // Cookies
+        // Sessions
+        // Filters
+        // Filters Advanced
+        // Callback Functions
+        // JSON
+        // Exceptions
+
+        // OOP
+        // Classes/Objects
+        // Constructor
+        // Destructor
+        // Access Modifiers
+        // Inheritance
+        // Constants
+        // Abstract Classes
+        // Interfaces
+        // Traits
+        // Static Methods
+        // Static Properties
+        // Namespaces
+        // Iterables
+
+    ?>
+</body>
+</html>
