bac1ca874f2c4e4085a4c6e3e737c0fd614cd3d7
mejasonatkinson <me@jasonatkinson.co.uk>
2022-08-31 21:24:47 +0000

    add comments

---
+++ public/w3school/02-comments/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>w3schools Playground</title>
+</head>
+<body>
+    
+    <h1>w3schools Playground</h1>
+    <h2>Comments</h2>
+
+    <?php 
+
+        // This is a single line comment
+
+        /*
+
+        This is a
+        multiple line 
+        comment
+        
+        */
+
+    ?>
+
+</body>
+</html>
