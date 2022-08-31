f9e577d8ccd2be755a1000673dae0b89473106fa
mejasonatkinson <me@jasonatkinson.co.uk>
2022-08-31 21:25:13 +0000

    add output types

---
+++ public/w3school/04-outputs/02-print/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>w3schools Playground</title>
+</head>
+<body>
+    
+    <h1>w3schools Playground</h1>
+    <h2>Print</h2>
+
+    <?php 
+
+        print "Hello World" . "<br>";
+        // echo has no return value while print has a return value of 1 so it can be used in expressions.
+        // echo is marginally faster than print
+
+    ?>
+
+</body>
+</html>
